cad73a258a21b0fedbcc9ed097b7bae45d625da3
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2025-03-25 21:28:23 +0100

    working product

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,82 @@
-type Stowage = {
+type LastFlip = {
     taken: boolean;
-    saved_at: string;
+    at: string;
 };
 
-function lastFlip(): Stowage | null {
+function lastFlip(): LastFlip | null {
     const value = localStorage.getItem("last_flip");
     if (!value) {
         return null;
     }
-    return JSON.parse(value);
+    const lastFlip: LastFlip = JSON.parse(value);
+    const before = new Date(lastFlip.at);
+    const now = new Date();
+
+    console.log(now.getDate() !== before.getDate());
+    if (
+        now.getMonth() !== before.getMonth() ||
+        now.getDate() !== before.getDate()
+    ) {
+        lastFlip.taken = false;
+    }
+    return lastFlip;
 }
 
-function saveFlip(taken: boolean) {
-    const value = localStorage.getItem("last_flip");
-    if (!value) {
-        return null;
+function saveFlip(lastFlip: LastFlip) {
+    localStorage.setItem("last_flip", JSON.stringify(lastFlip));
+}
+
+type RenderOptions = {
+    takenAt: HTMLParagraphElement;
+    taken: HTMLInputElement;
+    lever: HTMLElement;
+};
+
+function render(
+    storage: LastFlip,
+    { takenAt, taken, lever }: RenderOptions,
+) {
+    takenAt.textContent = format(storage.at);
+    taken.checked = storage.taken;
+    lever.classList.remove("lever-on", "lever-off");
+    lever.classList.add(storage.taken ? "lever-on" : "lever-off");
+}
+
+function format(input: string) {
+    function pad(num: number) {
+        const val = num.toString();
+        return val.length < 2 ? `0${val}` : val;
     }
-    return JSON.parse(value);
+    const date = new Date(input);
+
+    return `${pad(date.getDate())}/${pad(date.getMonth() + 1)} ${
+        pad(date.getHours())
+    }:${pad(date.getMinutes())}`;
 }
 
 function main() {
-    const checkbox = document.querySelector<HTMLInputElement>("#checkbox");
-    if (!checkbox) {
+    const taken = document.querySelector<HTMLInputElement>("#taken");
+    const takenAt = document.querySelector<HTMLParagraphElement>("#taken-at");
+    const lever = document.querySelector<HTMLElement>("#lever");
+    if (!taken || !takenAt || !lever) {
         throw new Error("unreachable: defined in index.html");
     }
 
-    checkbox.addEventListener("input", () => {
-        console.log("yep!", checkbox.checked);
+    taken.addEventListener("input", () => {
+        const at = new Date().toISOString();
+        const lastFlip: LastFlip = {
+            at,
+            taken: taken.checked,
+        };
+        saveFlip(lastFlip);
+        render(lastFlip, { takenAt, taken, lever });
     });
 
     const last = lastFlip();
     if (!last) {
+        return;
     }
+    render(last, { takenAt, taken, lever });
 }
 
 main();
